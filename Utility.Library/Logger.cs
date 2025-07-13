2138c53c84921cc4042dc0cbf441aef133afc8a1
victor <victorplu09@gmail.com>
2025-07-13 10:54:49 +0100

    add library + logger singleton

---
+++ Utility.Library/Logger.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+
+namespace Utility.Library
+{
+    public sealed class Logger
+    {
+        private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance => _instance.Value;
+
+
+        public Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+
+}
+
--- Utility/Program.cs
+++ Utility/Program.cs
@@ -1,34 +1,24 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Utility.Library;
 
 class Program
 {
     private static DiscordSocketClient _client;
     public static async Task Main()
     {
+        IConfiguration configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
         _client = new DiscordSocketClient();
 
-        _client.Log += Log;
+        _client.Log += Logger.Instance.Log;
 
-        //move to secrets
-        var token = "";
-
-        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-        // var token = File.ReadAllText("token.txt");
-        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+        string token = configuration["token"];
 
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
 
-        // Block this task until the program is closed.
         await Task.Delay(-1);
-    }
-
-    private static Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
     }
 
 }
